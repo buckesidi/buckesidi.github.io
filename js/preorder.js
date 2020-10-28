8b8af84ab596bc3d22cba638760dbff3a394edeb
neboj <nebojsa.kozlovacki@gmail.com>
2020-10-28 13:23:09 +0100

    Add serbian html form inputs translated msgs

--- js/preorder.js
+++ js/preorder.js
@@ -11,45 +11,52 @@ function showPreorderForm()
         <!-- Field type: "short" id: "1633920210" -->
         <fieldset>
             <legend for="1633920210">Ime i prezime</legend>
-            <input id="2005620554" placeholder="Ime i prezime" type="text" name="entry.2005620554" class="form-control" required>
+            <input id="2005620554" placeholder="Ime i prezime" type="text" name="entry.2005620554" class="form-control" oninvalid="this.setCustomValidity('Ovo polje je obavezno')"
+            oninput="this.setCustomValidity('')" required>
         </fieldset>
         
         <!-- Field type: "short" id: "235329302" -->
         <fieldset>
             <legend for="235329302">Država</legend>
-            <input id="835883743" placeholder="Država" type="text" name="entry.835883743" class="form-control" required>
+            <input id="835883743" placeholder="Država" type="text" name="entry.835883743" class="form-control" oninvalid="this.setCustomValidity('Ovo polje je obavezno')"
+            oninput="this.setCustomValidity('')" required>
         </fieldset>
 
 
         <!-- Field type: "short" id: "790080973" -->
         <fieldset>
             <legend for="790080973">Ulica i broj</legend>
-            <input id="1065046570" placeholder="Ulica i broj" type="text" name="entry.1065046570" class="form-control" required>
+            <input id="1065046570" placeholder="Ulica i broj" type="text" name="entry.1065046570" class="form-control" oninvalid="this.setCustomValidity('Ovo polje je obavezno')"
+            oninput="this.setCustomValidity('')" required>
         </fieldset>
         
         <!-- Field type: "short" id: "1265461668" -->
         <fieldset>
             <legend for="1265461668">Grad</legend>
-            <input id="476110520" placeholder="Grad" type="text" name="entry.476110520" class="form-control" required>
+            <input id="476110520" placeholder="Grad" type="text" name="entry.476110520" class="form-control" oninvalid="this.setCustomValidity('Ovo polje je obavezno')"
+            oninput="this.setCustomValidity('')" required>
         </fieldset> 
 
         <!-- Field type: "short" id: "1316787962" -->
         <fieldset>
             <legend for="1316787962">Poštanski broj</legend>
-            <input id="2134337608" placeholder="Poštanski broj" type="number" name="entry.2134337608" class="form-control" required>
+            <input id="2134337608" placeholder="Poštanski broj" type="number" name="entry.2134337608" class="form-control" oninvalid="this.setCustomValidity('Ovo polje je obavezno')"
+            oninput="this.setCustomValidity('')" required>
         </fieldset> 
 
      
         <!-- Field type: "short" id: "1770822543" -->
         <fieldset>
             <legend for="1770822543">Telefon</legend>
-            <input id="1166974658" placeholder="Telefon" type="text" name="entry.1166974658" class="form-control" required>
+            <input id="1166974658" placeholder="Telefon" type="text" name="entry.1166974658" class="form-control" oninvalid="this.setCustomValidity('Ovo polje je obavezno')"
+            oninput="this.setCustomValidity('')" required>
         </fieldset>
         
         <!-- Field type: "short" id: "227649005" -->
         <fieldset>
             <legend for="227649005">Email</legend>
-            <input id="1045781291" placeholder="Email" type="email" name="entry.1045781291" class="form-control" required>
+            <input id="1045781291" placeholder="Email" type="email" name="entry.1045781291" class="form-control" oninvalid="this.setCustomValidity('Ovo polje je obavezno')"
+            oninput="this.setCustomValidity('')" required>
         </fieldset>
 
         <fieldset>
