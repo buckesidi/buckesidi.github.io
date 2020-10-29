a98c291d420a5567c710519e1fd87bd15de5f36b
neboj <nebojsa.kozlovacki@gmail.com>
2020-10-29 10:27:02 +0100

    Define form before calling event listener

--- js/preorder.js
+++ js/preorder.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const flipCard = document.querySelector('.flip-card');
-    const heroTitle = document.getElementById('hero__title');
-    flipCard.addEventListener('click', showPreorderForm);
-    heroTitle.addEventListener('click', showPreorderForm);
-
     function showPreorderForm()
     {
         $('#formModal').modal('show');
     }
+
+    const flipCard = document.querySelector('.flip-card');
+    const heroTitle = document.getElementById('hero__title');
+    flipCard.addEventListener('click', showPreorderForm);
+    heroTitle.addEventListener('click', showPreorderForm);
 });
