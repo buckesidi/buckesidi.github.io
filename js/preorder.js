701076d2c1e7e70a6f1175fc835f73050979fb8c
neboj <nebojsa.kozlovacki@gmail.com>
2020-10-30 21:55:25 +0100

    Gallery change

--- js/preorder.js
+++ js/preorder.js
@@ -9,4 +9,13 @@ document.addEventListener('DOMContentLoaded', function() {
     const heroTitle = document.getElementById('hero__title');
     flipCard.addEventListener('click', showPreorderForm);
     heroTitle.addEventListener('click', showPreorderForm);
+
+    const smallImages = document.querySelectorAll('.vinyl-image.small');
+    const bigImage = document.querySelector('.vinyl-image.big');
+    smallImages.forEach(function(smallImg){
+        let imgSrc = smallImg.getAttribute('src');
+        smallImg.addEventListener('click',function(ev){
+            bigImage.src = imgSrc;
+        });
+    })
 });
