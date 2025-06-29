6c81006689fac25a18ceab8f23615225dab92327
Nebojsa Kozlovacki <nebojsa@Marijanas-MacBook-Pro-2.local>
2025-06-29 14:37:59 +0200

    arena zagreb redirect and new event tracking

--- theme/static/js/main.js
+++ theme/static/js/main.js
@@ -1,38 +1,82 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const ANALYTICS_EVENTS = {
+    TEST: "TEST",
+    ARENA_ZAGREB_CLICKED: "ARENA_ZAGREB_CLICKED",
+  };
+
   // Unix timestamp (in seconds) to count down to
-  console.log('<3');
+  console.log("<3");
   // var countDownDate = new Date('January 19, 2023 10:00:00').getTime();
   // var twoDaysFromNow = (new Date().getTime() / 1000) + (86400 * 2) + 1;
   // const containerEl = document.querySelector('.container');
-  const containerMid = document.getElementById('container-mid');
-  const containerLow = document.getElementById('container-low');
-  const albumsEl = document.querySelector('.albums');
-  const albumElements = document.querySelectorAll('.album');
+  const containerMid = document.getElementById("container-mid");
+  const containerLow = document.getElementById("container-low");
+  const albumsEl = document.querySelector(".albums");
+  const albumElements = document.querySelectorAll(".album");
   const hospulAlbum = albumElements[0];
   const euforijaAlbum = albumElements[1];
-  const buyBtns = document.querySelectorAll('.album .buy-btn');
+  const buyBtns = document.querySelectorAll(".album .buy-btn");
 
   const toggleContent = () => {
-    if (containerMid) {
-      containerMid.style.display = 'none';
+    // if (containerMid) {
+    //   containerMid.style.display = "none";
+    // }
+    // if (containerLow) {
+    //   containerLow.style.display = "none";
+    // }
+    const titleEl = document.getElementById("title-container-low");
+    const mainEl = document.getElementsByTagName("main");
+    if (mainEl[0]) {
+      mainEl[0].style.cursor = "pointer";
     }
-    if (containerLow) {
-      containerLow.style.display = 'none';
+    titleEl.innerText = "KUPI ULAZNICE";
+    titleEl.style.cursor = "pointer";
+    document.onclick = () => {
+      console.log("redirecting to event");
+      let hasRedirectedSuccessfully = false;
+      gtag("event", ANALYTICS_EVENTS.ARENA_ZAGREB_CLICKED, {
+        event_callback: () => {
+          hasRedirectedSuccessfully = true;
+          location.href =
+            "https://core-event.co/en/events/buc-kesidi-u-areni-zagreb-d646/";
+        },
+      });
+
+      // Osiguranje da se redirect desi i ako Google Analytics zakuca
+      setTimeout(() => {
+        if (!hasRedirectedSuccessfully) {
+          location.href =
+            "https://core-event.co/en/events/buc-kesidi-u-areni-zagreb-d646/";
+        }
+      }, 2000);
+    };
+    const flipdownLow = document.getElementById("flipdown-low");
+    const flipdownMid = document.getElementById("flipdown-mid");
+    if (flipdownMid) {
+      flipdownMid.style.display = "none";
+    }
+    if (flipdownLow) {
+      console.log(flipdownLow);
+      flipdownLow.style.display = "none";
     }
 
-    console.log("Redirecting to https://bfan.link/moderne-veze")
-    location.href="https://bfan.link/moderne-veze";
+    // console.log("Redirecting to https://bfan.link/moderne-veze")
+    // location.href="https://bfan.link/moderne-veze";
   };
   // const startDate = new Date('June 30, 2025 11:00:00');
-  const startDate = new Date('June 30, 2025 11:00:00');
+  const startDate = new Date("June 30, 2025 11:00:00");
 
   if (new Date() > startDate) {
     toggleContent();
   }
   var countDownDate = startDate.getTime() / 1000;
 
+  containerLow.onclick = () => {
+    gtag("event", ANALYTICS_EVENTS.TEST);
+  };
+
   // Set up FlipDown
-  var flipdown = new FlipDown(countDownDate, 'flipdown-low')
+  var flipdown = new FlipDown(countDownDate, "flipdown-low")
 
     // Start the countdown
     .start()
@@ -42,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
       toggleContent();
     });
 
-  var flipdown2 = new FlipDown(countDownDate, 'flipdown-mid')
+  var flipdown2 = new FlipDown(countDownDate, "flipdown-mid")
 
     // Start the countdown
     .start()
